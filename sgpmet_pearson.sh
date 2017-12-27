b7efe325498cc0538cc4d5834a7bef629be7f397
YupingLu <yupinglu89@gmail.com>
2017-12-27 15:20:44 -0500

    update shell script

--- sgpmet_pearson.sh
+++ sgpmet_pearson.sh
@@ -9,8 +9,8 @@ array=(1 3 4 5 6 7 8 9 11 13 15 20 21 24 25 27 31 32 33 34 35 36 37 38 39 40 41)
 
 for number in ${array[@]}
 do
-	path="/data/archive/sgp/sgpmetE"$number".b1"
-	inst="E"$number
+    path="/data/archive/sgp/sgpmetE"$number".b1"
+    inst="E"$number
     for year in {1993..2017}
     do
     	python3 sgpmet_pearson.py $path $inst $year
